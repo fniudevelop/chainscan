5a5abf9c243f764a590c5b95543fa2cfa88e9ce2
fniudevelop <112494633+fniudevelop@users.noreply.github.com>
2022-11-18 06:22:25 +0000

    update sql

---
+++ update.sql
@@ -0,0 +1,2 @@
+# Uniform Type Settings
+CREATE TABLE `types` ( `admin` SMALLINT(6) NOT NULL , `way` VARCHAR(100) NOT NULL , `content` VARCHAR(255) NOT NULL , `value` SMALLINT(1) NOT NULL ) ENGINE = InnoDB;
